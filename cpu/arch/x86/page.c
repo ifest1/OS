828a513a3b0ff278618eb0761d849fbc5113b06f
Iago Estrela <IagoFilipe.EstrelaBarros@windriver.com>
2021-10-17 00:06:21 -0300

    refactoring

--- cpu/arch/x86/page.c
+++ cpu/arch/x86/page.c
@@ -1,15 +1,17 @@
 #include <cpu/x86/page.h>
 
 void init_pgdir_entries(void) {
-    pgdir_table = (pg_t *) 0x00100000;
+    pgdir_table = (pg_t *) PAGE_TABLES_BASE_LOC;
+
     for (uint16_t i = 0; i < PAGE_DIR_ENTRY_SIZE; i++) {
         pgdir_table[i] = 0x00000002;
     }
 }
 
 void init_first_pg_table(void) {
-    first_pg_table = (pg_t *) (0x00100000 + (PAGES_TOTAL_SIZE));
+    first_pg_table = (pg_t *) (PAGE_TABLES_BASE_LOC + (PAGES_TOTAL_SIZE));
+
     for (uint16_t i = 0; i < PAGE_ENTRY_SIZE; i++) {
-        first_pg_table[i] = (i * 0x1000) | 3;
+        first_pg_table[i] = (i * 0x1000) | PRESENT | RW;
     }
 }
--- include/cpu/x86/page.h
+++ include/cpu/x86/page.h
@@ -1,9 +1,21 @@
 #include <stdint.h>
 #ifndef PAGE_H__
 #define PAGE_H__
+
+#define PAGE_TABLES_BASE_LOC                    0x00100000
 #define PAGE_DIR_ENTRY_SIZE                     1024
 #define PAGE_ENTRY_SIZE                         1024
 #define PAGES_TOTAL_SIZE                        4096
+
+#define PRESENT                                 1
+#define RW                                      2
+#define US                                      4
+#define PWT                                     8
+#define PCD                                     16
+#define ACCESSED                                32
+#define PAGE_SIZE                               64
+#define GLOBAL                                  128
+#define PAT                                     256
 
 typedef uint32_t                                pg_t;
 
